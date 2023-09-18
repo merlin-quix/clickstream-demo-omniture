6f03c6c0d18306a398e2b8a7b0a885d04974552b
Merlin Carter <merlin+demo@quix.io>
2023-09-18 11:17:28 +0000

    Edited 'main.py'

--- Enrich with Retail Categories/main.py
+++ Enrich with Retail Categories/main.py
@@ -1,12 +1,57 @@
 import quixstreams as qx
 import os
+import pandas as pd
+import redis
 
 # Quix injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
 client = qx.QuixStreamingClient()
 
 # Use Input / Output topics to stream data in or out of your service
-consumer_topic = client.get_topic_consumer(os.environ["input"])
-producer_topic = client.get_topic_producer(os.environ["output"])
+topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "lookup-data")
+topic_producer = client.get_topic_producer(os.environ["output"])
 
-# for more samples, please see samples or docs
+# Create a Redis client
+r = redis.Redis(
+  host='redis-11226.c304.europe-west1-2.gce.cloud.redislabs.com',
+  port=11226,
+  username='quixtut',
+  password='M3rl1n-06')
+
+# Function to fetch product name from Redis
+def get_product_name(product_id):
+    return r.get(product_id).decode('utf-8')
+
+def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+
+    # Transform data frame here in this method. You can filter data or add new features.
+    # Assume df is your dataframe and 'product_id' is the column with product IDs
+    df['product_name'] = df['col_13'].apply(get_product_name)
+    # Pass modified data frame to output stream using stream producer.
+    # Set the output stream id to the same as the input stream or change it,
+    # if you grouped or merged data with different key.
+    stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
+    stream_producer.timeseries.buffer.publish(df)
+
+
+# Handle event data from samples that emit event data
+def on_event_data_received_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
+    print(data)
+    # handle your event data here
+
+
+def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
+    # subscribe to new DataFrames being received
+    # if you aren't familiar with DataFrames there are other callbacks available
+    # refer to the docs here: https://docs.quix.io/sdk/subscribe.html
+    stream_consumer.events.on_data_received = on_event_data_received_handler # register the event data callback
+    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
+
+
+# subscribe to new streams being received
+topic_consumer.on_stream_received = on_stream_received_handler
+
+print("Listening to streams. Press CTRL-C to exit.")
+
+# Handle termination signals and provide a graceful exit
+qx.App.run()
